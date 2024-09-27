e3e2f62e03c57ba42bb27f172e29ce4c7fc3b803
kanade <59816838+kanade2019@users.noreply.github.com>
2024-09-27 20:15:44 +0800

    Add files via upload

--- drawing.py
+++ drawing.py
@@ -2,82 +2,37 @@ import turtle
 import math
 from PIL import Image
 
-l = 44
-r = l / math.sqrt(3)
-gap = 12
+l = 44 # length
+r = l / math.sqrt(3) # radius
+gap = 12 # gap length
+angle = math.pi / 2
 
+# setup the window
 screen = turtle.Screen()
 screen.setup(height=250, width=250)
 screen.bgcolor('white')
 
+# Create a turtle object
 t = turtle.Turtle()
 turtle.colormode(255)
-t.pencolor((51, 221, 170))
-t.pensize(1)
-t.speed(20)
+t.pencolor((51, 221, 170))  # Set the pen color
+t.pensize(1)  # Set the pen size
+t.speed(2)  # Set the drawing speed
 
-t.penup()
-t.goto(0, gap)
-t.left(60)
-t.pendown()
 t.fillcolor((51, 221, 170))
+# draw the flower petal
+for i in range(6):
+    t.left(60)
+    t.penup()
+    t.goto(gap * math.cos(angle), gap * math.sin(angle))
+    t.pendown()
+    t.begin_fill()
+    t.forward(l)
+    t.circle(r, 240)
+    t.forward(l)
+    t.end_fill()
+    angle = angle - math.pi/3
 
-t.begin_fill()
-t.forward(l)
-t.circle(r, 240)
-t.forward(l)
-# t.goto(0, 10)
-t.end_fill()
-
-t.penup()
-t.goto(gap/2 * math.sqrt(3), gap/2)
-t.pendown()
-t.begin_fill()
-t.left(60)
-t.forward(l)
-t.circle(r, 240)
-t.forward(l)
-t.end_fill()
-
-t.penup()
-t.goto(gap/2 * math.sqrt(3), -gap/2)
-t.pendown()
-t.begin_fill()
-t.left(60)
-t.forward(l)
-t.circle(r, 240)
-t.forward(l)
-t.end_fill()
-
-t.penup()
-t.goto(0, -gap)
-t.pendown()
-t.begin_fill()
-t.left(60)
-t.forward(l)
-t.circle(r, 240)
-t.forward(l)
-t.end_fill()
-
-t.penup()
-t.goto(-gap/2 * math.sqrt(3), -gap/2)
-t.pendown()
-t.begin_fill()
-t.left(60)
-t.forward(l)
-t.circle(r, 240)
-t.forward(l)
-t.end_fill()
-
-t.penup()
-t.goto(-gap/2 * math.sqrt(3), gap/2)
-t.pendown()
-t.begin_fill()
-t.left(60)
-t.forward(l)
-t.circle(r, 240)
-t.forward(l)
-t.end_fill()
 
 r = gap/2 + l / math.sqrt(3)
 
@@ -85,7 +40,7 @@ t.penup()
 t.pensize(3)
 t.goto(math.sqrt(3) / 2 * r, 1.5 * r)
 
-
+# draw the frame
 t.pendown()
 for i in range(6):
     t.left(60)
